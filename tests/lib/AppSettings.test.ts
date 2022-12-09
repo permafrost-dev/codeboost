e152d48bf000ba4b6d02bd7b77d5c869e30aafd2
patinthehat <patinthehat@users.noreply.github.com>
2022-12-09 16:02:50 +0000

    Fix styling

--- tests/lib/AppSettings.test.ts
+++ tests/lib/AppSettings.test.ts
@@ -9,13 +9,13 @@ const createSettings = (overrides: Partial<AppSettings> = {}): AppSettings => ({
 });
 
 it('should return the original settings if no environment variable references are found', () => {
-    const settings = createSettings({github_token: 'foo',});
+    const settings = createSettings({ github_token: 'foo' });
 
     expect(transformSettings(settings)).toEqual(settings);
 });
 
 it('should return the original settings if environment variable references are found but not set', () => {
-    const settings = createSettings({github_token: '$UNSET_ENV_VAR_NAME',});
+    const settings = createSettings({ github_token: '$UNSET_ENV_VAR_NAME' });
 
     expect(transformSettings(settings)).toEqual(settings);
 });
@@ -23,7 +23,7 @@ it('should return the original settings if environment variable references are f
 it('should replace environment variable references with their values', () => {
     process.env.TEST_ENV_VAR_NAME_FOO = 'bar';
 
-    const settings = createSettings({github_token: '$TEST_ENV_VAR_NAME_FOO',});
+    const settings = createSettings({ github_token: '$TEST_ENV_VAR_NAME_FOO' });
 
     expect(transformSettings(settings).github_token).toEqual('bar');
 });
