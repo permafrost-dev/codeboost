c234c6cd0401ccbfbc2b882ca076e6a700f2f619
Patrick Organ <patrick@permafrost.dev>
2022-12-11 19:17:55 -0500

    wip

--- dist/boosts/dependabot/boost.js
+++ dist/boosts/dependabot/boost.js
@@ -3,7 +3,7 @@ module.exports.default = {
     id: 'dependabot',
     version: '1.0.0',
     repository_limits: {
-        max_runs_per_version: 1,
+        max_runs_per_version: 999,
         minutes_between_runs: 60,
     },
     pull_request: {
