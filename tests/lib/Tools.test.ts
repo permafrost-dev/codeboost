23b080d16a4367baf4ec584ac8eb2eaf2c87d012
Patrick Organ <patrick@permafrost.dev>
2022-12-13 22:22:27 -0500

    add Tools unit tests

---
+++ tests/lib/Tools.test.ts
@@ -0,0 +1,71 @@
+import { Tools } from '@/lib/Tools';
+import { existsSync, readFileSync } from 'fs';
+
+let tools: Tools;
+
+beforeEach(() => {
+    tools = new Tools();
+});
+
+it('sleeps for 1 second', async () => {
+    const start = Date.now();
+    await tools.sleep(1000);
+    const end = Date.now();
+
+    expect(end - start).toBeGreaterThanOrEqual(1000);
+});
+
+it('checks if a file exists', () => {
+    expect(tools.fileexists(__filename)).toBeTruthy();
+    expect(tools.fileexists('non-existent-file')).toBeFalsy();
+});
+
+it('reads a file', () => {
+    expect(tools.readfile(__filename)).toContain(`it('reads a file', () => {`);
+});
+
+it('writes a file', () => {
+    const path = `${__dirname}/../temp/test.txt`;
+    const content = 'Hello, world!';
+
+    tools.writefile(path, content);
+
+    expect(existsSync(path)).toBeTruthy();
+    expect(readFileSync(path, 'utf-8')).toBe(content);
+});
+
+it('copies a file', () => {
+    const src = __filename;
+    const dest = `${__dirname}/../temp/test.ts`;
+
+    expect(existsSync(dest)).toBeFalsy();
+
+    tools.copyfile(src, dest);
+
+    expect(existsSync(dest)).toBeTruthy();
+});
+
+it('hashes a file', () => {
+    const path = `${__dirname}/../fixtures/test-boost-1/1.0.0/first.js`;
+
+    expect(tools.hashfile(path)).toBe('ebfa68fa54781a061bc26dde44e10a82df2bc1ded4aff93df82a505f364eb553');
+});
+
+it('compares two files', () => {
+    const path1 = `${__dirname}/../fixtures/test-boost-1/1.0.0/first.js`;
+    const path2 = `${__dirname}/../fixtures/test-boost-1/1.0.0/second.js`;
+
+    expect(tools.filesAreEqual(path1, path2)).toBeTruthy();
+});
+
+it('reads a yaml file', () => {
+    const path = `${__dirname}/../fixtures/files/test1.yaml`;
+
+    expect(tools.readYaml(path)).toMatchSnapshot();
+});
+
+it('reads a json file', () => {
+    const path = `${__dirname}/../fixtures/files/test1.json`;
+
+    expect(tools.readYaml(path)).toMatchSnapshot();
+});
