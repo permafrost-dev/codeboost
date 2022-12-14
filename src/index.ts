7ad7422a55c1fe1f3f8a5a8dca51fd16eafbde56
Patrick Organ <patrick@permafrost.dev>
2022-12-14 11:37:28 -0500

    sort exports

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export * from '@/lib/HistoryManager';
 export * from '@/lib/Repository';
 export * from '@/lib/Tools';
 export * from '@/lib/github';
+export * from '@/traits/HasLogger';
 export * from '@/types/BoostConfiguration';
 export * from '@/types/BoostHistory';
 export * from '@/types/RepositoryInfo';
-export * from '@/traits/HasLogger';
