bde83a0b62d39f14c948674f6b06bd42270a5f51
Patrick Organ <patrick@permafrost.dev>
2022-12-14 03:27:28 -0500

    wip

--- dist/boosts/dependabot/1.0.0/init.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@/lib/Boost').BoostScriptHandler} */
-module.exports.handler = async function (params) {
-    params.boost.log('initializing boost...');
-    //
-    params.boost.log('finished initializing boost');
-};
