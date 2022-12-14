28212f8fec77551f967cded0016d74bbffb74260
Patrick Organ <patrick@permafrost.dev>
2022-12-13 22:53:22 -0500

    wip

--- tests/lib/Repository.test.ts
+++ tests/lib/Repository.test.ts
@@ -1,4 +1,14 @@
 import { Repository } from '@/lib/Repository';
+import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+
+const tempPath = `${__dirname}/../fixtures/temp`;
+
+beforeEach(() => {
+    if (existsSync(`${tempPath}/laravel/framework`)) {
+        execSync(`rm -rf ${tempPath}/laravel/framework`);
+    }
+});
 
 it('sets properties correctly on create', async () => {
     const repository = new Repository('owner/name', 'path');
@@ -10,4 +20,24 @@ it('sets properties correctly on create', async () => {
     expect(repository.path).toBe('path/owner/name');
 
     expect(repository).toMatchSnapshot();
+});
+
+it('clones a repository', async () => {
+    const repository = new Repository('laravel/framework', tempPath);
+
+    await repository.clone();
+
+    expect(existsSync(`${__dirname}/../fixtures/temp/laravel/framework`)).toBeTruthy();
+    expect(existsSync(`${__dirname}/../fixtures/temp/laravel/framework/.git`)).toBeTruthy();
+});
+
+it('gets the default and current branch names', async () => {
+    const repository = new Repository('laravel/framework', tempPath);
+
+    await repository.clone();
+
+    const mainBranchName = await repository.defaultBranch();
+    const branch = await repository.currentBranch();
+
+    expect(branch).toBe(mainBranchName);
 });
