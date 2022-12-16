e0db906cc8b523126cef2910143345a4907656bc
Patrick Organ <patrick@permafrost.dev>
2022-12-16 18:32:45 -0500

    refactoring

--- src/lib/AppSettings.ts
+++ src/lib/AppSettings.ts
@@ -14,42 +14,39 @@ export interface AppSettings {
 }
 
 export function loadSettings(filename: string): AppSettings {
-    const settings = require(filename).default;
-
-    return transformSettings(settings);
+    return transformSettings(require(filename).default);
 }
 
+function loadLogTarget(settings) {
+    if (!Array.isArray(settings.log_target)) {
+        settings.log_target = [ settings.log_target ];
+    }
+
+    return (settings.log_target = settings.log_target
+        .map(target => <LogTarget>target.toLowerCase())
+        .filter((target: string) => [ 'console', 'file' ].includes(target)));
+}
+
+const isString = (value: any) => typeof value === 'string';
+const isUndefined = (value: any) => typeof value === 'undefined';
+
 export function transformSettings(settings: AppSettings): AppSettings {
+    const getEnvVar = (key: string) => process.env[settings[key].slice(1)];
+    const setDefaultValue = (key: string, value: any) => isUndefined(settings[key]) && (settings[key] = value);
+
     // load settings from environment variable references starting with '$'
     for (const key in settings) {
-        if (typeof settings[key] === 'string' && settings[key].startsWith('$')) {
-            if (typeof process.env[settings[key].slice(1)] !== 'undefined') {
-                settings[key] = process.env[settings[key].slice(1)];
-            }
+        if (isString(settings[key]) && !!getEnvVar(key)) {
+            settings[key] = getEnvVar(key);
         }
     }
 
-    if (!Array.isArray(settings.log_target)) {
-        settings.log_target = [settings.log_target];
-    }
-
-    settings.log_target = settings.log_target
-        .map(target => <LogTarget>target.toLowerCase())
-        .filter((target: string) => {
-            return ['console', 'file'].includes(target);
-        });
-
-    if (typeof settings.dry_run === 'undefined') {
-        settings.dry_run = false;
-    }
-
-    if (typeof settings.auto_merge_pull_requests === 'undefined') {
-        settings.auto_merge_pull_requests = false;
-    }
+    setDefaultValue('dry_run', false);
+    setDefaultValue('auto_merge_pull_requests', false);
 
     if (!settings.use_pull_requests) {
         settings.use_forks = false;
     }
 
-    return settings;
+    return loadLogTarget(settings);
 }
